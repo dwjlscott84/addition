74bb8bc802f0e93d9bdffb82822a35b397789744
dwjlscott84 <83475911+dwjlscott84@users.noreply.github.com>
2021-05-03 16:11:49 -0500

    Primary changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'addition';
+  sum: number = 0;
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+
+  sumNumbers() {
+    this.sum = this.firstNumber + this.secondNumber;
+  }
+
+  resetNumbers() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.sumNumbers();
+  }
 }
